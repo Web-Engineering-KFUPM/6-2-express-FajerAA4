2181194880d2a817b8d74b33d6c7a93f44fbc9dc
Fajer <s202280160@kfupm.edu.sa>
2025-11-10 11:25:44 +0300

    todo5.2

--- 6-2-node-express/backend/server.js
+++ 6-2-node-express/backend/server.js
@@ -2,6 +2,8 @@ import express from "express";
 import cors from "cors";
 import { getRandomQuote } from "./quotes.js";
 
+const app=express();
+const PORT =3000;
 {/*write code for cors*/}
 app.use(cors());
 app.get("/", (req, res) => {
@@ -9,11 +11,12 @@ app.get("/", (req, res) => {
 });
 
 {/*write code to define routes*/}
-
+app.get("/api/quote", (req, res) => {
+    const quote = getRandomQuote();
+    res.json({ quote });
+});
 
 {/*write code to create server*/}
-const app=express();
-const port =3000;
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
